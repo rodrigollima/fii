0c2819f3a4d2ebd34b19d75805c265c09acaf983
Rodrigo Lima <rodrigoxone@gmail.com>
2019-10-04 14:55:43 -0300

    Permission

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-until psql $DATABASE_URL -c '\l'; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1
-done
+#until psql $DATABASE_URL -c '\l'; do
+#    >&2 echo "Postgres is unavailable - sleeping"
+#    sleep 1
+#done
 
 >&2 echo "Postgres is up - continuing"
 
